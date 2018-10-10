8b0d6ffadebe61cac141ba10e04a25d5564d08fd
Jul Jen <juliajen.prog@gmail.com>
2018-10-10 05:16:11 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
-const input = document.querySelector('input');
+document.getElementById("myBtn").addEventListener("click", displayDate);
 
-input.addEventListener('keydown', function(e) {
-  if (e.which === 71) {
-    console.log('default prevented');
-    return e.preventDefault();
-  } else {
-    console.log('Not a "g"');
-  }
-});
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
+
